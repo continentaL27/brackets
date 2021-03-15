5d6e205891326b623e9d123b1d98d8d83469a3a2
Dmitry Gorbatenko <dimagorbatencko@gmail.com>
2021-03-15 04:48:02 +0300

    feat: Ñmethod implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+    const stack = [],
+          array = str.split("");
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+
+            if (array[i] === bracketsConfig[j][0]) {
+
+                if (bracketsConfig[j][0] === bracketsConfig[j][1]
+                    && stack[stack.length - 1] === bracketsConfig[j][0]) {
+                    stack.pop();
+                } else {
+                    stack.push(array[i]);
+                }
+            } else if (array[i] === bracketsConfig[j][1]) {
+
+                if (stack[stack.length - 1] !== bracketsConfig[j][0]) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            } else {
+                // do nothing
+            }
+        }
+    }
+    return (stack.length === 0);
 }
